2c839e063e7c797e4ddde1dfa04dc11ce9ff9a6f
VarHug <490641570@qq.com>
2018-05-05 17:06:44 +0800

    footer组件布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    
+    <bua-footer class="footer"></bua-footer>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import footer from './components/footer/footer';
+
 export default {
   data() {
     return {
 
-    }
+    };
   },
   components: {
-
+    'bua-footer': footer
   }
 };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import './common/stylus/mixin.styl'
 
+  .footer
+    position fixed
+    bottom 0
+    border-1px-top(rgba(7, 17, 27, 0.1))
 </style>
-
---
+++ src/components/footer/footer.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tab">
+    <div class="tab-item">
+      <i class="icon-twitter icon"></i>
+      <div class="text">消息</div>
+    </div>
+    <div class="tab-item">
+      <i class="icon-bubbles2 icon"></i>
+      <div class="text">话题</div>
+    </div>
+    <div class="tab-item">
+      <i class="icon-hammer icon"></i>
+      <div class="text">报修</div>
+    </div>
+    <div class="tab-item">
+      <i class="icon-user-tie icon"></i>
+      <div class="text">我的</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display flex
+    width 100%
+    height 50px
+    background-color rgb(240, 248, 255)
+    .tab-item
+      flex 1
+      text-align center
+      .icon
+        display block
+        padding-top 10px
+        line-height 15px
+        height 15px
+        font-size 15px
+      .text
+        line-height 25px
+        height 25px
+        font-size 12px
+</style>
