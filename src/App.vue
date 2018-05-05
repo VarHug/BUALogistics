7b765ffb8750b6101e9a535238e0e91a973e2251
VarHug <490641570@qq.com>
2018-05-05 14:04:46 +0800

    Vue-cli项目搭建

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
+
