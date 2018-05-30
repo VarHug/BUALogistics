3c70a658c7fe42d65230cbf5e6137831b494d891
VarHug <490641570@qq.com>
2018-05-30 21:10:27 +0800

    update user

---
+++ src/components/confirm/confirm.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="confirm-wrapper" v-show="show">
+    <div class="confirm">
+      <h1 class="title">操作提示</h1>
+      <div class="mes">{{message}}</div>
+      <div class="block-wrapper">
+        <span class="cancel" @click="blockCancel">取消</span>
+        <span class="sure" @click="blockConfirm">确定</span>
+      </div>
+      <i class="icon-close" @click="blockCancel"></i>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    message: {
+      type: String
+    },
+    show: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    blockCancel() {
+      this.$emit('cancel');
+    },
+    blockConfirm() {
+      this.$emit('confirm');
+    }
+  },
+  components: {
+
+  }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+.confirm-wrapper
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height 100%
+  background-color rgba(7,17,27,0.4)
+  .confirm
+    position absolute
+    top 50%
+    left 50%
+    transform translate(-50%, -50%)
+    padding 10px 0
+    width 250px
+    background-color #fff
+    border-radius 5px
+    .title
+      margin 0 0 10px 10px
+      line-height 18px
+      font-size 16px
+      color #666
+    .mes
+      margin-bottom 10px
+      padding 8px 0
+      text-align center
+      border-top 1px solid rgba(7, 17, 27, 0.1)
+      border-bottom 1px solid rgba(7, 17, 27, 0.1)
+    .block-wrapper
+      text-align center
+      font-size 0
+      .cancel, .sure
+        display inline-block
+        padding 8px
+        font-size 13px
+        border 1px solid rgba(7, 17, 27, 0.1)
+        border-radius 5px
+      .sure
+        margin-left 15px
+        background-color rgba(0,160,220,.2)
+        color #4d555d
+    .icon-close
+      position absolute
+      top 5px
+      right 5px
+      padding 3px
+      font-weight 700
+      color #999
+</style>
--- src/components/mine/mine.vue
+++ src/components/mine/mine.vue
@@ -16,18 +16,20 @@
       <div class="find-content">
         <h1 class="title">{{getFindText}}</h1>
         <ul v-show="user.userFind && user.userFind.length !== 0">
-          <li v-for="(find,index) in user.userFind" :key="index">
-            <div class="find-desc">{{find.desc}}</div>
-            <div class="find-state">{{getFindState(find.state)}}</div>
+          <li class="find-item" v-for="(find,index) in user.userFind" :key="index">
+            <span class="find-state" :class="{'gray': find.state === 1 || find.state === 3}" @click="findStateClick(find)">{{getFindState(find.state)}}</span>
+            <p class="find-desc">{{find.desc}}</p>
           </li>
         </ul>
       </div>
     </div>
+    <v-confirm :message="'您确定物品已经找回了么'" :show="confirmShow" @cancel="confirmCancel" @confirm="confirmSure"></v-confirm>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import split from '../split/split';
+import confirm from '../confirm/confirm';
 
 const ERR_OK = 0;
 const STATE_LOSE = 0; // 寻找中
@@ -38,7 +40,10 @@ const STATE_BACK = 3; // 已归还
 export default {
   data() {
     return {
-      user: []
+      user: [],
+      findConfirmTxt: '',
+      confirmShow: false,
+      curFind: {}
     };
   },
   created() {
@@ -59,19 +64,38 @@ export default {
       } else if (state === STATE_BACK) {
         return '已归还';
       }
+    },
+    findStateClick(find) {
+      if (find.state === STATE_LOSE) {
+        this.findConfirmTxt = '您确定物品已经找回了么';
+      } else if (find.state === STATE_DRAW) {
+        this.findConfirmTxt = '您确定物品已经归还了么';
+      } else {
+        return;
+      }
+      this.curFind = find;
+      this.confirmShow = true;
+    },
+    confirmCancel() {
+      this.confirmShow = false;
+    },
+    confirmSure() {
+      this.curFind.state++;
+      this.confirmShow = false;
     }
   },
   computed: {
     getFindText() {
       if (this.user.userFind && this.user.userFind.length !== 0) {
-        return '您的“失物招领相关信息如下”';
+        return '您的“失物招领”相关信息如下';
       } else {
         return '您还未有“失物招领”的相关信息哦';
       }
     }
   },
   components: {
-    split
+    split,
+    'v-confirm': confirm
   }
 };
 </script>
@@ -100,6 +124,32 @@ export default {
     .find-content
       padding 18px 18px 0
       .title
+        margin-bottom 8px
         line-height 14px
         font-size 14px
+        color #07111b
+      .find-item
+        display flex
+        margin-bottom 10px
+        padding 0 12px
+        .find-state
+          flex 0 0 60px
+          box-sizing border-box
+          display inline-block
+          vertical-align top
+          margin-right 10px
+          width 60px
+          height 30px
+          padding 8px 12px
+          font-size 12px
+          color #ffffff
+          border-radius 1px
+          background-color #00a0dc
+          &.gray
+            background-color #4d555d
+        .find-desc
+          display inline-block
+          vertical-align top
+          line-height 16px
+          font-size 12px
 </style>
